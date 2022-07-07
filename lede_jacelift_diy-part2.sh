360704b176d5239b1f5bc8245f7dba459c8b9da1
jacelift <74590290+jacelift@users.noreply.github.com>
2022-07-07 19:40:44 +0800

    Update lede_jacelift_diy-part2.sh

--- lede_jacelift_diy-part2.sh
+++ lede_jacelift_diy-part2.sh
@@ -16,71 +16,9 @@
 
 # 【1】添加wt600机型到lede源码中（不依赖luci-app-mt-wifi）2022.6.14
 # lede_RX_WT600_2022.6.14 make from cudy_wr1300 and newifi-d2
-chmod 755 -R files/*
-
-#【1.1】修改文件mt7621.mk：add wt600 to target/linux/ramips/image/mt7621.mk
-wt600_mt7621.mk(){
-
-define Device/d-team_newifi-d2
-  $(Device/uimage-lzma-loader)
-  IMAGE_SIZE := 32448k
-  DEVICE_VENDOR := Newifi
-  DEVICE_MODEL := D2
-  DEVICE_COMPAT_VERSION := 1.1
-  DEVICE_PACKAGES := kmod-mt7603e kmod-mt76x2e kmod-usb3 \
-	kmod-usb-ledtrig-usbport luci-app-mtwifi -wpad-openssl
-endef
-TARGET_DEVICES += d-team_newifi-d2
-
-define Device/RX_WT600
-  $(Device/uimage-lzma-loader)
-  IMAGE_SIZE := 32448k
-  DEVICE_VENDOR := RX
-  DEVICE_MODEL := WT600
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 kmod-usb3 \
-	kmod-usb-ledtrig-usbport
-endef
-TARGET_DEVICES += RX_WT600
-
-}
-sed -i '/TARGET_DEVICES += d-team_newifi-d2/a\\ndefine Device\/RX_WT600\n  \$(Device\/uimage-lzma-loader)\n  IMAGE_SIZE := 32448k\n  DEVICE_VENDOR := RX\n  DEVICE_MODEL := WT600\n  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 kmod-usb3 \\\n	kmod-usb-ledtrig-usbport\nendef\nTARGET_DEVICES += RX_WT600' target/linux/ramips/image/mt7621.mk
+# chmod 755 -R files/*
 
 
-#【1.2】修改文件01_leds：add wt600 to target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-#==========================================
-#在指定字符前后添加内容：https://www.cnblogs.com/bulh/articles/11071783.html
-#例如：在1111之前添加AAA,方法如下：
-#sed -i 's/指定的字符/要插入的字符&/'  文件
-#[root@localhost ~]# sed -i  's/1111/AAA&/' /tmp/input.txt                     
-#[root@localhost ~]# cat /tmp/input.txt                   
-#null
-#0000AAA11112222
-#在1111之后添加BBB，方法如下：
-#sed -i 's/指定的字符/&要插入的字符/'  文件
-#[root@localhost ~]# sed -i  's/1111/&BBB/' /tmp/input.txt    
-#[root@localhost ~]# cat /tmp/input.txt                   
-#null
-#0000AAA1111BBB2222
-#==========================================
-#在指定字符后添加内容:sed -i 's/d-team,newifi-d2/&|\\\nRX,WT600/' target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-#在指定字符前添加内容:
-sed -i 's/d-team,newifi-d2/RX,WT600|\\\n&/' target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-
-#【1.3】修改文件02_network：add wt600 to target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-#在指定字符前添加内容：https://www.cnblogs.com/bulh/articles/11071783.html
-#sed -i 's/d-team,newifi-d2/&|\\\n	RX,WT600/' target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-#===============【修正LAN口顺序】====================
-rm -r target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-cp -f files/RX_WT600/02_network-test target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-#===============【修正LAN口顺序】====================
-#sed -i 's/	d-team,newifi-d2/	RX,WT600|\\\n&/' target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-
-#【1.4】新增文件并修改mt7621_RX_WT600.dts：cp target/linux/ramips/dts/mt7621_cudy_wr1300.dts target/linux/ramips/dts/mt7621_RX_WT600.dts
-#sed替换字符串操作，多个替换可以在同一条命令中执行,用分号隔开即可“;”。
-#sed -i 's/cudy,wr1300/RX,WT600/g;s/Cudy WR1300/RX WT600/g' target/linux/ramips/dts/mt7621_RX_WT600.dts
-cp -f files/lede/RX_WT600/mt7621_RX_WT600.dts target/linux/ramips/dts/mt7621_RX_WT600.dts
-
-#===================================================================================================
 
 
 #【2】更改openwrt的主机名为WT600，Modify hostname
